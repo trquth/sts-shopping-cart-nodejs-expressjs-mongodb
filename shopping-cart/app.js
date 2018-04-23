0d14f815d456f18960c7e844522353c8a94dc2a2
trquth <tranquthien@gmail.com>
2018-04-23 14:24:22 +0700

    Configure session for cart

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -5,11 +5,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
-var mongoose =  require('mongoose');
+var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -24,17 +25,25 @@ require('./config/passport');
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', expressHbs({
-  defaultLayout : 'layout',
-  extname : '.hbs'
+  defaultLayout: 'layout',
+  extname: '.hbs'
 }))
 app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret :'mysupersecret' ,resave : false, saveUninitialized : false}));
+app.use(session({
+  secret: 'mysupersecret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  }),
+  cookie: {maxAge: 180 * 60 * 1000}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -42,18 +51,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 })
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
