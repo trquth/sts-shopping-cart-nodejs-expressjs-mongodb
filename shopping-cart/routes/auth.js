97d6c25c031ac96d6de27e4f6d743e1066d7a273
trquth <tranquthien@gmail.com>
2018-04-27 08:47:35 +0700

    Save code

--- shopping-cart/routes/auth.js
+++ shopping-cart/routes/auth.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
+var fs = require('fs');
+
+var dataDir = __dirname + '/data';
+var avatarImageDir = dataDir + '/avatar-photo';
+fs.existsSync(dataDir) || fs.mkdirSync(dataDir);
+fs.existsSync(avatarImageDir) || fs.mkdirSync(avatarImageDir);
+
+function saveContentEntry(photoPath) {
+
+}
 
 router.get('/register-account', (req, res, next) => {
     res.render('auth/register-account')
@@ -9,7 +19,7 @@ router.get('/register-account', (req, res, next) => {
 
 router.post('/register-account', (req, res, next) => {
     if (req.xhr || req.accepts('json,html') === 'json') {
-        res.send({ success: true, type :'success', message : 'Call AJAX successfully' })
+        res.send({success: true, type: 'success', message: 'Call AJAX successfully'})
     } else {
         res.redirect('/register-account')
     }
@@ -35,10 +45,19 @@ router.post('/register-account/upload-file', (req, res, next) => {
     var form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
         if (err) {
+            req.session.flash = {
+                type: 'danger',
+                message: 'Error during uploading image'
+            }
             return res.redirect('/auth/register-account/upload-file')
         }
+        var photo = files.photo
+        var dir = `${avatarImageDir}/${Date().now()}`
+        
     })
-    res.redirect('/auth/register-account/upload-file')
+
+  
+   // res.redirect('/auth/register-account/upload-file')
 })
 
 router.use('/upload', (req, res, next) => {
